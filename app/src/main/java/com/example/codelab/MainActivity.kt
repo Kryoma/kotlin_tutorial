5f36e4cd0d823b1957a8759989e1e2745344ae1a
Kryoma <ryoma.kuboshi@gmail.com>
2022-11-23 16:57:58 +0900

    画像追加

--- app/src/main/java/com/example/codelab/MainActivity.kt
+++ app/src/main/java/com/example/codelab/MainActivity.kt
@@ -4,21 +4,25 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.codelab.ui.theme.CodeLabTheme
 
@@ -32,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    HappyBirthdayWithText(message = "お誕生日おめでとう!", from = "久保ちゃんより")
+                    BirthdayGreetingWithImage(message = "お誕生日おめでとう!", from = "久保ちゃんより")
                 }
             }
         }
@@ -40,9 +44,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(),
+            contentScale = ContentScale.Crop
+        )
+        HappyBirthdayWithText(message = message, from = from)
+    }
+
+}
+
+@Composable
 fun HappyBirthdayWithText(message: String, from: String) {
     Column {
-        Text(text = message, fontSize = 36.sp)
+        Text(
+            text = message,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .padding(start = 14.dp, top = 110.dp)
+        )
         val offset = Offset(5.0f, 10.0f)
         Text(
             text = from,
@@ -53,14 +81,22 @@ fun HappyBirthdayWithText(message: String, from: String) {
                     offset = offset,
                     blurRadius = 3f
                 )
-            )
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp, top = 6.dp)
         )
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun BirthDayCardPreview() {
     CodeLabTheme {
-        HappyBirthdayWithText(message = "お誕生日おめでとう", from = "久保ちゃんより")
+        BirthdayGreetingWithImage(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.from_text)
+        )
     }
 }
